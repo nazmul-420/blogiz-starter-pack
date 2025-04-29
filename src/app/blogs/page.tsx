79a427576eaa4eaa881a5d91d180096ace35c22c
nazmul-2020 <nazmulcpi2012@gmail.com>
2025-04-29 17:01:03 +0600

    rtk query use & client side data lode

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,11 +1,16 @@
+"use client";
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 import { Blog } from "@/type";
 
-const AllBlogs = async () => {
-  const res = await fetch("http://localhost:5000/blogs", {
-    cache: "no-store",
-  });
-  const blogs = await res.json();
+const AllBlogs = () => {
+  // const res = await fetch("http://localhost:5000/blogs", {
+  //   cache: "no-store",
+  // });
+  // const blogs = await res.json();
+  const { data: blogs, isLoading, isError, error } = useGetBlogsQuery("");
+  console.log(blogs);
+
   return (
     <div className="w-[90%] mx-auto">
       <h1 className="text-center text-4xl my-5">All Blogs Service </h1>
@@ -16,7 +21,7 @@ const AllBlogs = async () => {
         </i>
       </p>
       <div className="grid grid-cols-3 gap-4 my-6">
-        {blogs.map((blog: Blog) => (
+        {blogs?.map((blog: Blog) => (
           <BlogCard blog={blog} key={blog.id} />
         ))}
       </div>
